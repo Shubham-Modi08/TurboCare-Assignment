aa30c5b8b55f50fae9cb5c117c2af788acb0b739
shubh <mailshubham8995@gmail.com>
2021-05-16 00:46:19 +0530

    added custom vehicle profile screen

--- app/src/main/java/com/shubham/turbocare_assignment/MainActivity.kt
+++ app/src/main/java/com/shubham/turbocare_assignment/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.shubham.turbocare_assignment
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var title:TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_main)
+
+        val actionBar = supportActionBar
+        actionBar!!.hide()
+
+        title = findViewById(R.id.title)
+        title.setText(getString(R.string.create_profile) )
+
     }
+
+
+
 }
+
+
