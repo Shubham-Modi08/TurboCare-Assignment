75c19e596c85f5aade00facf8470b4476f4a820d
shubh <mailshubham8995@gmail.com>
2021-05-17 22:36:49 +0530

    Added the my VehicleList

--- app/src/main/java/com/shubham/turbocare_assignment/activities/VehicleList.kt
+++ app/src/main/java/com/shubham/turbocare_assignment/activities/VehicleList.kt
@@ -8,8 +8,12 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import androidx.room.Room
 import com.shubham.turbocare_assignment.R
+import com.shubham.turbocare_assignment.adapters.VlAdapter
+import com.shubham.turbocare_assignment.adapters.VmAdapter
 import com.shubham.turbocare_assignment.database.VehicleDatabase
 import com.shubham.turbocare_assignment.database.VehicleEntity
 
@@ -18,6 +22,10 @@ class VehicleList : AppCompatActivity() {
 
     private lateinit var title: TextView
     private lateinit var addVehicle: Button
+    private lateinit var rcv: RecyclerView
+    private lateinit var VlAdapter: VlAdapter
+    private var list: ArrayList<VehicleEntity> = ArrayList()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +34,7 @@ class VehicleList : AppCompatActivity() {
         addVehicle = findViewById(R.id.add_vehicle)
         title = findViewById(R.id.title)
         title.text = getString(R.string.vehicle_list)
+        initView()
 
         addVehicle.setOnClickListener {
             intent = Intent(applicationContext, MainActivity::class.java)
@@ -35,14 +44,25 @@ class VehicleList : AppCompatActivity() {
 
         val result = DBASyncTask(this,1).execute().get()
         Log.d("result", result.toString())
-
-
-
+        //VlAdapter = VlAdapter(,this)
+        rcv.layoutManager = LinearLayoutManager(this)
+        rcv.adapter = VlAdapter
 
 
 
     }
+
+    private fun initView() {
+        rcv = findViewById(R.id.recycler_my_vehicle)
+    }
+
+    private fun vehicleList(vehicle : ArrayList<VehicleEntity>){
+        list = vehicle
+    }
+
     class DBASyncTask(context: Context, private val mode:Int) : AsyncTask<Void, Void, Boolean>(){
+
+        val list = ArrayList<VehicleEntity>()
 
         private val db = Room.databaseBuilder(context, VehicleDatabase::class.java,"vehicle_details-db").build()
 
@@ -51,9 +71,13 @@ class VehicleList : AppCompatActivity() {
             return when(mode){
                 1 -> {
                     val vehicleEntity:List<VehicleEntity>? = db.getVehicleDao().getMyVehicles()
-                    Log.d("data1",vehicleEntity.toString())
+                    Log.d("data1", vehicleEntity?.get(0).toString())
                     db.close()
-                    vehicleEntity !=null
+                    vehicleEntity?.forEach {
+                        list.add(it)
+                    }
+                    Log.d("list",list.toString())
+                    vehicleEntity != null
                 }
                 else -> false
             }
---
+++ app/src/main/java/com/shubham/turbocare_assignment/adapters/VlAdapter.kt
@@ -0,0 +1,53 @@
+package com.shubham.turbocare_assignment.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.shubham.turbocare_assignment.R
+import com.shubham.turbocare_assignment.database.VehicleEntity
+
+class VlAdapter(private val list: ArrayList<VehicleEntity>, val listener: MyOnClickListener): RecyclerView.Adapter<VlAdapter.MyViewHolder>(){
+
+    inner class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        var vehicleRegno = itemView.findViewById<TextView>(R.id.registration_no)
+        var vehicleModel = itemView.findViewById<TextView>(R.id.vehicle_model)
+        init {
+            itemView.setOnClickListener {
+                val position = adapterPosition
+                listener.OnClick(position)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_my_vehicle,parent,false)
+        return MyViewHolder(view)
+    }
+
+
+
+
+    override fun onBindViewHolder(holder: VlAdapter.MyViewHolder, position: Int) {
+        val item = list[position]
+        holder.apply {
+            vehicleRegno.text = item.Reg_no
+            vehicleModel.text = item.Vehicle_model
+        }
+    }
+
+
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    interface MyOnClickListener{
+        fun OnClick(position: Int)
+    }
+
+
+
+
+}
